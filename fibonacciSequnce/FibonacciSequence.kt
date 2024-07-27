a8552cc7645a708d26991f50984e33f051078969
UycGitHub <umut.yusuf.cinar.mail@gmail.com>
2024-07-27 17:02:38 +0300

    Added: FibonacciSequence.kt added.

---
+++ fibonacciSequnce/FibonacciSequence.kt
@@ -0,0 +1,26 @@
+/*
+    Problem: FibonacciSequence
+    We will write it recursively.
+    However, problems solved with recursion where large numbers are used as input
+    may cause the program to crash or performance losses due to the windows opened in memory due
+    to the function being called too many times.
+    For this reason, we will not use a recursive function in our next example,
+    the factorial calculation program. We will solve it with a different solution method.
+ */
+package fibonacciSequnce
+
+fun main(){
+    val input = readln().toInt()
+    println(fibonacci(input))
+}
+
+fun  fibonacci(n: Int): Int {
+    if(n<0) {
+        println("Incorrect input")
+        return -1
+    }
+    else {
+        if (n <= 1) return n
+        return fibonacci(n - 1) + fibonacci(n - 2)
+    }
+}
