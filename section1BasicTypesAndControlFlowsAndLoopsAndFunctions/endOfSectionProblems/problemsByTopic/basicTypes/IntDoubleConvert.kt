2ddb4729994da485049836fbf1d3c8178e8c0a62
UycGitHub <umut.yusuf.cinar.mail@gmail.com>
2024-08-27 12:30:01 +0300

    Added: IntDoubleConvert.kt file's codes added.

--- section1BasicTypesAndControlFlowsAndLoopsAndFunctions/endOfSectionProblems/problemsByTopic/basicTypes/IntDoubleConvert.kt
+++ section1BasicTypesAndControlFlowsAndLoopsAndFunctions/endOfSectionProblems/problemsByTopic/basicTypes/IntDoubleConvert.kt
@@ -1,2 +1,28 @@
 package section1BasicTypesAndControlFlowsAndLoopsAndFunctions.endOfSectionProblems.problemsByTopic.basicTypes
 
+fun main(){
+    println("Which type of conversion do you want to do?" +
+            "\n1. Int to Double" + "\n2. Double to Int" + "Enter 1 or 2")
+    val convertType = readln()
+    if (convertType !in listOf("1", "2")) {
+        println("Invalid choice")
+        return
+    }
+    else {
+        when (convertType) {
+            "1" -> {
+                println("Enter an integer")
+                val int = readln().toInt()
+                val double = int.toDouble()
+                println("The double value is $double")
+            }
+
+            "2" -> {
+                println("Enter a double")
+                val double = readln().toDouble()
+                val int = double.toInt()
+                println("The integer value is $int")
+            }
+        }
+    }
+}
