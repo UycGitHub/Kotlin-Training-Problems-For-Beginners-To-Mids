be81f04e69dd8f295c073fc9697b8503edb2dc17
UycGitHub <umut.yusuf.cinar.mail@gmail.com>
2024-08-31 21:13:25 +0300

    Changed: LetterGrade.kt Kotlin file content has been changed. Since the file is under the ProblemsByTopic/controlFlows directory, the function usage in the code of the IfElseCarSpeed file has been removed. Only the if - else and when structures were used and the operation was not divided into a separate function. It was written inside the main() function.

--- section1BasicTypesAndControlFlowsAndLoopsAndFunctions/endOfSectionProblems/problemsByTopic/controlFlows/LetterGrade.kt
+++ section1BasicTypesAndControlFlowsAndLoopsAndFunctions/endOfSectionProblems/problemsByTopic/controlFlows/LetterGrade.kt
@@ -3,16 +3,18 @@ package section1BasicTypesAndControlFlowsAndLoopsAndFunctions.endOfSectionProble
 fun main() {
     println("Enter your grade")
     val grade = readln().toInt()
-    println("Your grade and result is ${letterGrade(grade)}")
+    val result = if(grade<=100){
+        when {
+            grade >= 90 -> "A, Excellent"
+            grade >= 80 -> "B,  Good"
+            grade >= 70 -> "C, Average"
+            grade >= 60 -> "D,  Passing"
+            else -> "F, Fail"
+        }
+    } else {
+        "Please enter a valid grade"
+    }
+    println("Your grade and result is $result")
 }
 
-fun letterGrade(grade: Int): String {
-    return when {
-        grade >= 90 -> "A, Excellent"
-        grade >= 80 -> "B,  Good"
-        grade >= 70 -> "C, Average"
-        grade >= 60 -> "D,  Passing"
-        else -> "F, Fail"
-    }
-}
 
