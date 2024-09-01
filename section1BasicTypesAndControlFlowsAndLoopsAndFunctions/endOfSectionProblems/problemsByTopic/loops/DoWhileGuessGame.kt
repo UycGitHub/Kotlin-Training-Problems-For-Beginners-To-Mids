b69a4af84d61c7fe026de8f4bbfcd6287b980eb7
UycGitHub <umut.yusuf.cinar.mail@gmail.com>
2024-09-01 09:04:00 +0300

    Added: DoWhileGuessGame.kt file added.

---
+++ section1BasicTypesAndControlFlowsAndLoopsAndFunctions/endOfSectionProblems/problemsByTopic/loops/DoWhileGuessGame.kt
@@ -0,0 +1,27 @@
+/*
+Problem: Make a guessing game.
+Description: You will make a simple game that asks the user to guess a number.
+The computer will randomly choose a number between 1 and 100.
+The user must enter a guess repeatedly until the number is correctly guessed.
+Use a "do-while" loop to repeat the game each time the user enters a guess until the guess is correct.
+The program should print "Too high" if the guess is too high,
+"Too low" if the guess is too low, and
+"You got it!" if the guess is correct.
+ */
+package section1BasicTypesAndControlFlowsAndLoopsAndFunctions.endOfSectionProblems.problemsByTopic.loops
+
+import kotlin.random.Random
+
+fun main() {
+    val numberToGuess = Random.nextInt(1, 101)
+    var userGuess: Int
+    do {
+        print("Enter your guess: ")
+        userGuess = readln().toInt()
+        when {
+            userGuess > numberToGuess -> println("Too high")
+            userGuess < numberToGuess -> println("Too low")
+            else -> println("You got it!")
+        }
+    } while (userGuess != numberToGuess)
+}
