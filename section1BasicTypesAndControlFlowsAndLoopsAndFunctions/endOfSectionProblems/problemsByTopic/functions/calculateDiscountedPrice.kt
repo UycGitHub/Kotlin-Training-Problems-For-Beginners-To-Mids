043a89f2bdf027e3db30ac4323bc552341708271
UycGitHub <umut.yusuf.cinar.mail@gmail.com>
2024-09-08 21:09:13 +0300

    Added: calculateDiscountedPrice.kt file added.

---
+++ section1BasicTypesAndControlFlowsAndLoopsAndFunctions/endOfSectionProblems/problemsByTopic/functions/calculateDiscountedPrice.kt
@@ -0,0 +1,20 @@
+/*
+    Basic function definition and calling:
+    Write a function called calculateDiscountedPrice that takes two Double parameters called price and
+    discount and returns a Double.
+    The function should return the discounted price of the item.
+    The discounted price is calculated by subtracting the discount percentage from the price.
+    For example, if the price is 100.0 and the discount is 0.1, the discounted price would be 90.0.
+    The function should return the discounted price.
+ */
+package section1BasicTypesAndControlFlowsAndLoopsAndFunctions.endOfSectionProblems.problemsByTopic.functions
+
+fun main() {
+    val priceInput = readln().toDouble()
+    val discountInput = readln().toDouble()
+    println(calculateDiscountedPrice(priceInput, discountInput))
+}
+
+fun calculateDiscountedPrice(price: Double, discount: Double): Double {
+    return price - (price * discount)
+}
